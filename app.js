3299862cb4878620e8b9e6a1a6780e1914c38979
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-24 23:01:01 -0800

    Added play again button, and winning number is now randomly generated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ GAME FUNCTION:
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI elements
@@ -24,6 +24,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e) {
+    if(e.target.className === 'play-again') {
+        window.location.reload();
+    }
+});
 
 // Listen for guess
 guessBtn.addEventListener('click', function() {
@@ -65,7 +72,17 @@ function gameOver(won, msg) {
     guessInput.style.borderColor = color;
     // Set message
     setMessage(msg, color);
+
+    // Play again
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
 }
+
+// Get winning number
+function getRandomNum(min, max) {
+    return Math.floor((Math.random() * (max - min + 1) + min));
+}
+
 
 function setMessage(msg,color) {
     message.style.color = color;
